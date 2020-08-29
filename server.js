e6dd54045ca572688580d69218efd54097d1f32e
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-29 11:26:30 -0400

    import tables class, add routes for pages and api links

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const Tables = require('./classes/tables');
+
+const reservations = [];
+const tables = [];
+const waitlist = [];
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
+
 app.get('/', (req, res) => {
-    const message = "this is our server";
-    res.send(message);
-})
+    // sends index/homepage
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/tables', (req, res) => {
+    // sends page with tables
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/reserve', (req, res) => {
+    // sends page to make a reservation
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+// Displays waitlist
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
 
 
 // Starts the server to begin listening
